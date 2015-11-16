6bb7085ce35f0a34c1eb65a16f5061e7afaec881
layupbolon <layupbolon@sina.com>
2015-11-16 14:23:59 +0800

    1.添加获取微信access_token的代码 2.添加TestApp项目，并测试上述代码

---
+++ Framework.Core/TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Framework.Core.Wechat;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WechatPush wechatPush = new WechatPush();
+            string token = wechatPush.GetAccessToken();
+            Console.WriteLine(token);
+            Console.ReadKey();
+        }
+    }
+}
